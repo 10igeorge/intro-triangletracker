5cbf922fef56115f9fbe428dce782e93287526a4
10igeorge <10izzygeorge@gmail.com>
2016-01-18 20:19:34 -0800

    finalize jququery functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,34 @@
 var triangleTracker = function(sideA, sideB, sideC) {
+  var sideA = parseInt($('input#sideA').val());
+  var sideB = parseInt($('input#sideB').val());
+  var sideC = parseInt($('input#sideC').val());
+
   if ((sideA === sideB) && (sideB === sideC)) {
-    return "equilateral";
+    $('#triangleResult').show();
+    return "equilateral ";
   } else if ((sideA === sideB) && (sideA !== sideC)) {
-    return "isosceles";
-  } else if ((sideA !== sideB) && (sideB !== sideC) && (sideA !== sideC)) {
-    return "scalene";
+    $('#triangleResult').show();
+    return "isosceles ";
+  } else if ((sideA !== sideB) && (sideA !== sideC) && (sideB !== sideC)) {
+    $('#triangleResult').show();
+    return "scalene ";
   } else {
     return "Your side lengths do not make a valid triangle.";
   }
 }
+
+
+
+$(document).ready(function() {
+  $('.finalTriangle').click(function(event) {
+    var result = triangleTracker(sideA, sideB, sideC);
+
+    $('.triangle').text(result);
+
+
+
+    event.preventDefault();
+  });
+
+
+});
