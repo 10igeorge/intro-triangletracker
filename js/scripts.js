3ff3e9ad09d141adc83010cee827bfb4b5abdb70
10igeorge <10izzygeorge@gmail.com>
2016-01-18 19:46:22 -0800

    add isosceles functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var triangleTracker = function(sideA, sideB, sideC) {
   if ((sideA === sideB) && (sideB === sideC)) {
     return "equilateral";
+  } else if ((sideA === sideB) && (sideA !== sideC)) {
+    return "isosceles";
+  } else {
+    return "Your side lengths do not make a valid triangle.";
   }
 }
--- specs/specs.js
+++ specs/specs.js
@@ -2,5 +2,7 @@ describe('triangleTracker', function() {
   it("will have equal side lengths", function() {
     expect(triangleTracker(30, 30, 30)).to.equal("equilateral");
   });
-
+  it("will have two equal sides", function() {
+    expect(triangleTracker(30, 30, 10)).to.equal("isosceles");
+  });
 });
